ec33ccab2265ec844076c59d6b1774301f960e90
Binayak Tiwari <binayaktiwari@gmail.com>
2018-09-23 19:16:50 -0700

    update

--- traffic/copy_trace.sh
+++ traffic/copy_trace.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# Copies the trace file to the location of the simulation, i.e, location of the popnet simulator
+#Copies the trace file to the location of the simulation, i.e, location of the popnet simulator
 cp -r ../trace ~/Desktop/8x8/Hier/popnet/
 cp -r ../trace ~/Desktop/8x8/Multipath/popnet/
 cp -r ../trace ~/Desktop/8x8/Unicast/popnet/
